9ec3dd5b58954ebe876a1025fd9401b9e8b22a55
Thomas Deutsch <thomas@tuxpeople.org>
2021-01-27 20:38:57 +0100

    Update usb-poweroff.sh

--- scripts/usb-poweroff.sh
+++ scripts/usb-poweroff.sh
@@ -20,4 +20,5 @@ sudo uhubctl -a off -l ${location} -p ${port}
 sudo udevadm trigger --action=remove /sys/bus/usb/devices/${location}.${port}/
 
 # this is needed to work around bug in octoprint
-sudo service octoprint restart 
+sudo service octoprint stop 
+sudo service octoprint start 
